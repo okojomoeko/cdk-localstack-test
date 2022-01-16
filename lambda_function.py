326ac7b4f71c39de8b912ab19302b8e005430329
okojomoeko <chikoro.size@gmail.com>
2022-01-16 19:26:19 +0900

    Lambdaをlocalstackで試す

---
+++ lambda_function.py
@@ -0,0 +1,25 @@
+import boto3
+import os
+
+LOCALSTACK_IP = os.environ['LOCALSTACK_IP']
+
+# localstackのendpointを指定
+
+s3 = boto3.resource(
+    aws_access_key_id='dummy',
+    aws_secret_access_key='dummy',
+    region_name='ap-northeast-1',
+    service_name='s3',
+    endpoint_url = f'http://{LOCALSTACK_IP}:4566'
+)
+
+def lambda_handler(event, context):
+    bucket = 'test-bucket'    # バケット名を指定
+    key = 'test.txt'
+
+    res = s3.Bucket(bucket).Object(key).get()
+    body = res['Body'].read()
+    return {
+        "statusCode": 200,
+        "body": body,
+    }
